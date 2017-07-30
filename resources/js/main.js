0cb1166441e12cec112a4819cbfda15278ce1bbd
Inna <designbyinna@gmail.com>
2017-07-30 13:53:56 -0600

    separate js file

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+  $("a.dropdown-toggle").click(function(ev) {
+            $("a.dropdown-toggle").dropdown("toggle");
+            return false;
+        });
+        $("ul.dropdown-menu a").click(function(ev) {
+            $("a.dropdown-toggle").dropdown("toggle");
+            return false;
+});
